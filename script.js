1818c22ef5f1d96b1af71fbfe8cd9ed08b16e247
Nitesh Dhakad <110675128+Nitesh1234445@users.noreply.github.com>
2025-11-21 20:59:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+let text = document.getElementById('text');
+let delay = document.getElementById('delay');
+let btn = document.getElementById('btn');
+let output = document.getElementById('output');
+
+async function promisefnc(delayTime, message){
+	try{
+	return new Promise((res, rej) => {
+		setTimeout(() => {
+			res(message);
+		},delayTime);
+	})
+  }catch(err){
+		return err;
+  }
+}
+btn.addEventListener("click" , async ()=> {
+	let delayTime = Number(delay.value);
+	let msg = text.value;
+	let result = await promisefnc( delayTime, msg);
+	output.textContent = result;
+})
+
+
